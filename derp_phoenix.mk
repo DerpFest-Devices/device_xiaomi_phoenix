eccd8ba30bc687f4f8e3152a085c25c35e947644
sreekfreak995 <sreekfreak995@gmail.com>
2020-08-13 18:39:45 +0530

    phoenix: get derped
    Signed-off-by: sreekfreak995 <sreekfreak995@gmail.com>

--- derp_phoenix.mk
+++ derp_phoenix.mk
@@ -7,14 +7,16 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_phoenix
+PRODUCT_NAME := derp_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
+DERP_BUILDTYPE := Official
+IS_PHONE := true
 
 # PixelExperience Properties
 TARGET_GAPPS_ARCH := arm64
