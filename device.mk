bc90c473bfdbc44ca8d4d8bb9290c02b833aaeba
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-05-23 19:54:59 +0530

    phoenix: import atfwd@2.0.policy from V12.1.3.0.RGHINXM

--- device.mk
+++ device.mk
@@ -476,6 +476,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/seccomp/atfwd@2.0.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/atfwd@2.0.policy \
     $(LOCAL_PATH)/configs/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/configs/seccomp/wfdservice.policy:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/seccomp_policy/wfdservice.policy
 
