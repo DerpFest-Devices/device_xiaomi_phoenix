850e3b85ed55817c3070092f67c757ab2afec0b2
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-10-15 19:07:19 +0530

    phoenix: Add shim for missing symbol in lib-imsvt.so
    * E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system_ext/lib64/lib-imsvideocodec.so"
    
    Signed-off-by: Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>

--- device.mk
+++ device.mk
@@ -581,6 +581,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Shims
+PRODUCT_PACKAGES += \
+    lib-imsvt
+
 # Soter
 PRODUCT_PACKAGES += \
     libhwbinder.vendor
